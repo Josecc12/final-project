e9b1a0ead71533c5e83f9a07da0725446ab214a5
Fernandavasquez <castilloferlisbeth@gmail.com>
2024-09-01 00:37:16 -0600

    rama kan-17 en proceso

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,9 +1,11 @@
+import { MedicineTable } from "@/components/medicine-table";
 import { ProductsTable } from "@/components/products-table";
 import { Test } from "@/components/test";
 
 export default function Home() {
   return <main className="min-h-[1000px]">
     <ProductsTable/>
+    <MedicineTable/>
   
     
   </main>;
