36ef2755e99487d8869114daeb6d600922d66047
Fernandavasquez <castilloferlisbeth@gmail.com>
2024-09-20 21:12:38 -0600

    Tabla Categoría realizada

---
+++ src/app/types/models/Category.ts
@@ -0,0 +1,9 @@
+
+
+type Category = {
+    id: string
+    nombre: string
+  
+}
+
+export default Category
--- src/components/navigation/MainNav.tsx
+++ src/components/navigation/MainNav.tsx
@@ -3,6 +3,7 @@ import { Button } from "../ui/button";
 
 import {
   ActivityIcon,
+  ChartBarStacked,
   ClipboardPlusIcon,
   HomeIcon,
   House,
@@ -62,6 +63,14 @@ export default function MainNav() {
           >
             <ShoppingBasketIcon className="h-5 w-5" />
             Tickets
+          </Link>
+          <Link
+            href="/category"
+            className="flex items-center gap-2 rounded-md px-3 py-2 text-sm font-medium text-gray-700 hover:bg-gray-200 hover:text-gray-900 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-50"
+            prefetch={false}
+          >
+            <ChartBarStacked  className="h-5 w-5" />
+            Categorías
           </Link>
         </nav>
       </div>
