c094eb07b43548fe06b8730373fa613e678e09fb
Rodolfoba1 <awirodolfo@gmail.com>
2024-09-28 19:37:48 -0600

    Eliminación de usuarios y categorías con onDelete

--- src/app/(main)/category/[id]/page.client.tsx
+++ src/app/(main)/category/[id]/page.client.tsx
@@ -1,49 +1,75 @@
-"use client";
+'use client';
 
-
-import { Category, User } from "@/app/types/models";
+import { Category } from "@/app/types/models";
 import LayoutSection from "@/components/LayoutSection";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Typography } from "@/components/ui/Typography";
-import Delete  from "@/components/ui/delete";
+import Delete from "@/components/ui/delete"; 
+import deleteCategory from "@/actions/category/delete"; 
 import { useRouter } from "next/navigation";
 import Link from "next/link";
+import { toast } from "@/components/ui/use-toast";
+import { ErrorResponse } from "@/app/types/api";
 
 type Props = {
   category: Category;
 };
 
 export default function PageClient({ category }: Props) {
+  const router = useRouter(); 
 
+  const onDelete = async () => {
+    const response = await deleteCategory({ id: category.id }); 
 
-  const router = useRouter();
-
+    if (response?.status === 200) {
+      toast({
+        title: "Categoría eliminada exitosamente",
+        description: `La categoría ${category.nombre} ha sido eliminada`,
+        variant: "default",
+      });
+      router.push("/category");
+      router.refresh(); 
+    } else if (response?.status === 401) {
+      toast({
+        title: "Error de autenticación",
+        description: "No estás autorizado para realizar esta acción.",
+        variant: "destructive",
+      });
+    } else if ("message" in response) {
+      toast({
+        duration: 3000,
+        title: `Error ${response.status}`,
+        description: (response as ErrorResponse).message,
+      });
+    }
+  };
 
   return (
-    <LayoutSection title={`Detalle de categoria`} description="Aqui puedes ver los detalles de la categoria" 
-    actions= {
-      <div className="flex gap-2 self-end">
-      <Button variant="default" asChild>
-        <Link href={`/category/${category.id}/edit`}>
-        Editar</Link>
-      </Button>
-      <Delete categoria={category}/>
-    </div>
-    }
-    
+    <LayoutSection
+      title={`Detalle de categoría`}
+      description="Aquí puedes ver los detalles de la categoría"
+      actions={
+        <div className="flex gap-2 self-end">
+          <Button variant="default" asChild>
+            <Link href={`/category/${category.id}/edit`}>
+              Editar
+            </Link>
+          </Button>
+          <Delete onDelete={onDelete} />
+        </div>
+      }
     >
       <div className="flex flex-col gap-2 xl:gap-3">
         <div className="w-full flex flex-col gap-1">
-          <Typography variant="small" className="font-bold">Codigo</Typography>
+          <Typography variant="small" className="font-bold">Código</Typography>
           <Typography variant="muted">{category.id}</Typography>
         </div>
         <div className="w-full flex flex-col gap-1">
           <Typography variant="small" className="font-bold">Nombre</Typography>
           <Typography variant="muted">{category.nombre}</Typography>
         </div>
-        <div className="w-full flex  items-center gap-1">
-        </div>
+        <div className="w-full flex items-center gap-1"></div>
       </div>
     </LayoutSection>
   );
