507c7a17073c4509c3ba47bcefd407464872c985
Rodolfoba1 <awirodolfo@gmail.com>
2024-10-02 18:30:59 -0600

    Implementacion de eliminacion de insumo

--- src/app/types/models/index.ts
+++ src/app/types/models/index.ts
@@ -1,7 +1,9 @@
 import Category from "./Category";
 import User from "./User";
 import Department from "./Department";
+import Inventory from "./Inventario";
 
 export type {User}
 export type {Category}
 export type {Department}
+export type {Inventory}
