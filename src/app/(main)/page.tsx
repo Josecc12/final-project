18fb03e019daaed69ddd299002eb1c058429e533
pablo <pablo.puac@dooply.io>
2024-11-11 10:51:38 -0600

    colores generales cambiados

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,17 +1,12 @@
 "use client";
-
-import DropdownSearch from "@/components/DropdownSearch";
-import Test from "./Test";
-
 export default function Home() {
   return (
-    <main className="min-h-[1000px] flex items-center justify-center">
-      <div className="max-w-[600px] w-full">
-        {/* Logo centrado y responsive */}
+    <main className="min-h-screen flex items-center justify-center">
+      <div className="max-w-xs sm:max-w-md md:max-w-lg lg:max-w-xl w-full">
         <img 
           src="/logo.png" 
           alt="Logo" 
-          className="mx-auto w-full sm:w-[200%] md:w-[200%] lg:w-[200%] h-auto" 
+          className="mx-auto w-full h-auto" 
         />
       </div>
     </main>
