8c4c8a89e0db88b23288f181ac305d754a4d6e16
Pablo David Puac García <pablopuacg@gmail.com>
2024-11-10 22:51:06 -0600

    icons

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -3,13 +3,17 @@
 import DropdownSearch from "@/components/DropdownSearch";
 import Test from "./Test";
 
-
-
 export default function Home() {
-  return <main className="min-h-[1000px]">
-    <div className="max-w-[600px]">
-    <Test />
-    
-    </div>
-  </main>;
+  return (
+    <main className="min-h-[1000px] flex items-center justify-center">
+      <div className="max-w-[600px] w-full">
+        {/* Logo centrado y responsive */}
+        <img 
+          src="/logo.png" 
+          alt="Logo" 
+          className="mx-auto w-full sm:w-[200%] md:w-[200%] lg:w-[200%] h-auto" 
+        />
+      </div>
+    </main>
+  );
 }
