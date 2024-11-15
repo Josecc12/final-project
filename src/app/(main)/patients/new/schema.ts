507ed3ca549f3da374f27d030dc0fd9d5c304c59
urruthia <urrutiapedro494gmail.com>
2024-11-14 19:53:47 -0600

    Agregndo campos opcionales de antecendetes al schema

--- src/app/(main)/patients/new/schema.ts
+++ src/app/(main)/patients/new/schema.ts
@@ -1,5 +1,7 @@
 import { z } from "zod";
 
+
+
 const schema = z.object({
     nombre: z.string().min(1, "El nombre es requerido"),
     sexo: z.string().min(1, "El sexo es requerido"),
@@ -10,7 +12,27 @@ const schema = z.object({
     traumaticos: z.string(),
     alergias: z.string(),
     vicios: z.string(),
- 
+
+
+    gestas: z.number().optional(),
+  hijos_vivos: z.number().optional(),
+  hijos_muertos: z.number().optional(),
+  abortos: z.number().optional(),
+  ultima_regla: z
+  .date()
+  .optional()
+  .refine(
+    (date) => date === undefined || date <= new Date(),
+    {
+      message: "La última regla no puede ser una fecha futura",
+    }
+  ),
+  planificacion_familiar: z.string().optional(),
+  partos: z.number().optional(),
+  cesareas: z.number().optional(),
+
+
+
    });
 
 
