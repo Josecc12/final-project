d48f40a2661c40c88af6be4db4de41d6bae31aae
Pablo David Puac García <pablopuacg@gmail.com>
2024-11-21 12:19:50 -0600

    se arreglo la busqueda

--- src/actions/department/findOne.ts
+++ src/actions/department/findOne.ts
@@ -5,23 +5,42 @@ import { ErrorResponse, SuccessReponse } from "../../app/types/api";
 import { cookies } from "next/headers";
 import { parsedEnv } from "@/app/env";
 import { Department } from "@/app/types/models";
+import parsePaginationParams from "@/utils/functions/parsePaginationParams";
+
+type Props = {
+  searchParams?: URLSearchParams;
+  q?: string; // Add optional search query parameter
+};
 
 export default async function findOne(
-  id: string
+  id: string,
+  props: Props = {}
 ): Promise<SuccessReponse<Department> | ErrorResponse> {
   try {
     const url = `${parsedEnv.API_URL}/departamentos/${id}`;
     const session = cookies().get("session")?.value;
+    const parsedParams = parsePaginationParams(props.searchParams);
     
     const response = await axios.get<Department>(url, {
       headers: {
         Authorization: `Bearer ${session}`,
       },
+      params: {
+        ...parsedParams,
+        query: undefined,
+        q: props.q || parsedParams.query, // Use the passed q parameter or existing query
+      }
     });
+
     return {
-      data: response.data, // data debe coincidir con la estructura esperada de Department
+      data: response.data,
       status: 200,
       statusText: response.statusText,
+      meta: {
+        totalItems: response.data.totalItems,
+        totalPages: response.data.totalPages,
+        page: response.data.page,
+      },
     };
   } catch (error) {
     if (isAxiosError(error)) {
--- src/app/(main)/department/[id]/page.tsx
+++ src/app/(main)/department/[id]/page.tsx
@@ -1,18 +1,20 @@
 "use client";
 import findOne from "@/actions/department/findOne";
 import PageClient from "./page.client";
-
+import { ReadonlyURLSearchParams } from "next/navigation";
 
 
 type Props = {
   params: {
       id: string;
   }
+  searchParams: ReadonlyURLSearchParams;
 }
 
 export default async function Page({params}:Props) {
 
   const response = await findOne(params.id)
+
   if (response.status !== 200 || !("data" in response)) {
     throw new Error("Failed to fetch user data");
   }
