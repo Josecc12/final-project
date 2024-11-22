cc9ae3512ba834fbfd710bdef5f336de89dc702b
Pablo David Puac García <pablopuacg@gmail.com>
2024-11-21 21:34:19 -0600

    crear un retiro de departamento
    ahora ya se puede crear un retiro de insumo por departamento

---
+++ src/actions/departures/create.ts
@@ -0,0 +1,60 @@
+"use server";
+
+import { parsedEnv } from "@/app/env";
+import axios, { isAxiosError } from "axios";
+import { cookies } from "next/headers";
+import { ErrorResponse, SuccessReponse } from "../../app/types/api";
+import { revalidatePath } from "next/cache";
+import { getSession } from "../auth";
+
+interface WithdrawalDto {
+    descripcion: string;
+    detalles: {
+        insumoId: string;
+        cantidad: number;
+    }[];
+}
+
+export default async function create({
+    descripcion,
+    detalles,
+}: WithdrawalDto): Promise<SuccessReponse<any> | ErrorResponse> {
+    try {
+        const url = `${parsedEnv.API_URL}/retiros`;
+        const session = cookies().get("session")?.value;
+        const sess = await getSession();
+        
+        const body = {
+            usuarioId: sess?.sub,
+            descripcion,
+            detalles,
+        };
+        console.log(body);
+        const response = await axios.post(url, body, {
+            headers: {
+                Authorization: `Bearer ${session}`,
+            },
+        });
+
+        revalidatePath("/departures");
+
+        return {
+            data: response.data,
+            status: response.status,
+            statusText: response.statusText,
+        };
+    } catch (error) {
+        console.log(error);
+        if (isAxiosError(error)) {
+            return {
+                message: error.message,
+                code: error.code,
+                status: error.response?.status,
+            };
+        } else {
+            return {
+                message: "An unexpected error occurred.",
+            };
+        }
+    }
+}
